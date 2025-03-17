4e640a4d8895dc421378ad14d87cd260f0c9aa5a
Toan <nguyencongtoana1nhvt@gmail.com>
2025-03-17 16:45:51 +0700

    Update

--- routes/frontend.php
+++ routes/frontend.php
@@ -3,27 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Frontend\PostController;
 use App\Http\Controllers\Frontend\AgencyController;
-use App\Http\Controllers\Frontend\HistoryController;
 use App\Http\Controllers\Frontend\HomeController;
-use App\Http\Controllers\Frontend\ContactController;
-use App\Http\Controllers\Frontend\CourseController;
-use App\Http\Controllers\Frontend\JobController;
-use App\Http\Controllers\Frontend\OrderController;
 use App\Http\Controllers\Frontend\ProductController;
 use App\Http\Controllers\Frontend\ServiceController;
 use App\Http\Controllers\Frontend\SitemapController;
-use App\Http\Controllers\Frontend\PolicyController;
-use App\Http\Controllers\Frontend\ResourceController;
-use Inertia\Inertia;
-
 
 Route::get('sitemap.xml', [SitemapController::class, 'index']);
-
-Route::get(Lang::uri('search-v2'), [HomeController::class, 'searchV2'])->name('api.search');
-
-Route::get(Lang::uri('search'), [HomeController::class, 'search'])->name('search');
-Route::get(Lang::uri('/contact'), [AgencyController::class, 'index'])->name('contact');
-
 
 Route::controller(ProductController::class)->group(function () {
     Route::get(Lang::uri('products'), 'index')->name('products.index');
