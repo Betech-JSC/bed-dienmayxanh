655f9beb621049b1c8c5ace60362dd5c46a2c164
Toan <nguyencongtoana1nhvt@gmail.com>
2025-05-21 14:31:09 +0700

    Update Post.php

--- app/Models/Post/Post.php
+++ app/Models/Post/Post.php
@@ -337,7 +337,7 @@ class Post extends BaseModel
             'description' => $this->description,
             'category' => $this->category,
             'categories' => $categories,
-            'images' => $images,
+            'images' => $this->images,
             'image' => $this->getImageDetail($this->image),
             'icon' => $this->getImageDetail($this->icon),
             'url' => $this->current_url,
